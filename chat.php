d3323d83c1e8012608391b2024eccfd525522718
eliyas <eliyasson11@gmail.com>
2023-12-15 21:20:48 +0200

    adjust chat on the user side

--- chat.php
+++ chat.php
@@ -1,36 +1,55 @@
 <?php
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
+
 if ($conn->connect_error) {
     die("Fatal connection Error");
 }
 
-if (isset($_POST['username'])) {
-    $username = get_post($conn, 'username');
-    $message = get_post($conn, 'message');
-    $timestamp = get_post($conn, 'timestamp'); // Corrected 'timetamp' to 'timestamp'
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get values from the form
+    $username = $_POST['username'];
+    $message = $_POST['message'];
+    
+    // Use the current timestamp for the database insertion
+    $timestamp = date('Y-m-d H:i:s'); // Format the timestamp as per your database column
 
+    // Insert data into the database
     $query = "INSERT INTO messages (username, message, timestamp) VALUES ('$username', '$message', '$timestamp')";
     $result = $conn->query($query);
+
     if (!$result) {
-        echo "INSERT failed<br><br>";
+        echo "INSERT failed: " . $conn->error;
     }
 }
 
-echo <<<_END
-<form action="chat.php" method="post"><pre>
-Username: <input type="text" name="username">
-Message: <textarea type="text" name="message"></textarea>
-Timestamp: <input type="text" name="timestamp">
-<input type="submit" value="send">
-</pre></form></center>
-_END;
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chat</title>
+</head>
+<body>
+
+<form action="chat.php" method="post">
+    <pre>
+        Username: <input type="text" name="username">
+        Message: <textarea name="message"></textarea>
+        <input type="submit" value="Send">
+    </pre>
+</form>
+
+<?php
+// Display the messages
 $query = "SELECT * FROM messages";
 $result = $conn->query($query);
+
 if (!$result) {
     die("Database access failed: " . $conn->error);
 }
+
 $rows = $result->num_rows;
 
 for ($j = 0; $j < $rows; ++$j) {
@@ -39,21 +58,17 @@ for ($j = 0; $j < $rows; ++$j) {
     $r1 = htmlspecialchars($row['username']);
     $r2 = htmlspecialchars($row['message']);
     $r3 = htmlspecialchars($row['timestamp']);
-    echo <<<_END
-    <pre>
-    Id: $r0
-    Username: $r1
-    Message: $r2
-    Timestamp: $r3
-    </pre>
-_END;
+    
+    echo "<pre>";
+    echo "Id: $r0<br>";
+    echo "Username: $r1<br>";
+    echo "Message: $r2<br>";
+    echo "Timestamp: $r3<br>";
+    echo "</pre>";
 }
 
 $result->close();
 $conn->close();
-
-function get_post($conn, $var)
-{
-    return $conn->real_escape_string($_POST[$var]);
-}
 ?>
+</body>
+</html>
--- newdevice.php
+++ newdevice.php
@@ -1,101 +1,100 @@
-<?php
-    require_once 'login.php';
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if ($conn->connect_error) {
-        die("Fatal connection Error");
-    }
-    if (isset($_POST['device'])) {
-        $device = get_post($conn, 'device');
-        $maker = get_post($conn, 'maker');
-        $model = get_post($conn, 'model');
-        $year = get_post($conn, 'year');
-        $stock = get_post($conn, 'stock');
-        $loans = get_post($conn, 'loans');
-        $query = "INSERT INTO devices (device, maker, model, year, stock, loans) VALUES ('$device', '$maker', '$model', '$year', '$stock', '$loans')";
 
-        $result = $conn->query($query);
-        if(!$result) echo "INSERT failed<br><br>";
-    }
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('location: signin.php');
+    exit();
+}
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Device Management</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file -->
+    <title>Home Page</title>
+    <link rel="stylesheet" href="tyylit2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="$
 </head>
-<body>
-    <div class="container">
-        <h1>Add a New Device</h1>
-        <form action="newdevice.php" method="post">
-            <div class="form-group">
-                <label for="device">Device</label>
-                <input type="text" name="device" id="device">
+<body style="isolatio: isolate;">
+    <div class="container py-3">
+        <header>
+            <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+                <a href="" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+                    <span class="fs-4">Mysqli Kirjasto</span>
+                </a>
+                <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="home.php">Lisää kirjoja</a>
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="newdevice.php">Lisää laitteita$
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="wishlist.php">Laitteiden Lainaus</a>
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="shat.php">Chat</a>
+                    <a class="py-2 link-body-emphasis text-decoration-none" href="logout.php">Logout</a>
+                </nav>
             </div>
-            <div class="form-group">
-                <label for="maker">Maker</label>
-                <input type="text" name="maker" id="maker">
-            </div>
-            <div class="form-group">
-                <label for="model">Model</label>
-                <input type="text" name="model" id="model">
-            </div>
-            <div class="form-group">
-                <label for="year">Year</label>
-                <input type="text" name="year" id="year">
-            </div>
-            <div class="form-group">
-                <label for="stock">Stock</label>
-                <input type="text" name="stock" id="stock">
-            </div>
-            <div class="form-group">
-                <label for="loans">Loans</label>
-                <input type="text" name="loans" id="loans">
-            </div>
-            
-            <input type="submit" value="ADD DEVICE" class="btn">
-        </form>
-        
-        <div class="device-info">
-            <?php
-                $query = "SELECT * FROM devices";
-                $result = $conn->query($query);
-            
-                if (!$result) {
-                    die("Database access failed: " . $conn->error);
-                }
-            
-                while ($row = $result->fetch_array(MYSQLI_NUM)) {
-                    $r0 = htmlspecialchars($row[0]);
-                    $r1 = htmlspecialchars($row[1]);
-                    $r2 = htmlspecialchars($row[2]);
-                    $r3 = htmlspecialchars($row[3]);
-                    $r4 = htmlspecialchars($row[4]);
-                    $r5 = htmlspecialchars($row[5]);
-                    
-                    echo "<center><pre>";
-                    echo "Device: $r0<br>";
-                    echo "Maker: $r1<br>";
-                    echo "Model: $r2<br>";
-                    echo "Year: $r3<br>";
-                    echo "Stock: $r4<br>";
-                    echo "Loans: $r5<br>";
-                    echo "<form action='deldevice.php' method='post'>";
-                    echo "<input type='hidden' name='delete' value='yes'>";
-                    echo "<input type='hidden' name='device' value='$r0'>";
-                    echo "<input type='submit' value='DELETE DEVICE'>";
-                    echo "</form>";
-                    echo "</pre></center>";
-                }
-            
-                $result->close();
-                $conn->close();
-                
-                function get_post($conn, $var) {
-                    return $conn->real_escape_string($_POST[$var]);
-                }
-            ?>
-        </div>
-    </div>
+        </header>
+    <h2>Tervetuloa, <?php echo $_SESSION['username']; ?>!</h2>
+</div>
+
+<?php
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Fatal Connection Error");
+if (isset($_POST['device'])) {
+    $device = get_post($conn, 'device');
+    $maker = get_post($conn, 'maker');
+    $model = get_post($conn, 'model');
+    $year = get_post($conn, 'year');
+    $stock = get_post($conn, 'stock');
+    $loans = get_post($conn, 'loans');
+    $query = "INSERT INTO devices (device, maker, model, year, stock, loans) VALUES ('$device', '$maker', '$model', '$year', '$stock', '$loans')";
+
+    $result = $conn->query($query);
+    if(!$result) echo "INSERT failed<br><br>";
+} echo <<<HTML
+
+<form action="newdevice.php" method="post"><pre>
+    Device  <input type="text" name="device">
+    Maker   <input type="text" name="maker">
+    Model   <input type="text" name="model">
+    Year    <input type="text" name="year">
+    Stock   <input type="text" name="stock">
+    Loans   <input type="text" name="loans">
+    <input type="submit" value="ADD DEVICE">
+</pre></form>
+HTML;
+$query = "SELECT * from devices";
+$result = $conn->query($query);
+if(!$result) die ("Database access failed");
+
+while ($row = $result->fetch_array(MYSQLI_NUM)) {
+    $r0 = htmlspecialchars($row[0]);
+    $r1 = htmlspecialchars($row[1]);
+    $r2 = htmlspecialchars($row[2]);
+    $r3 = htmlspecialchars($row[3]);
+    $r4 = htmlspecialchars($row[4]);
+    $r5 = htmlspecialchars($row[5]);
+    echo "<center><pre>";
+    echo "Device: $r0<br>";
+    echo "Maker: $r1<br>";
+    echo "Model: $r2<br>";
+    echo "Year: $r3<br>";
+    echo "Stock: $r4<br>";
+    echo "Loans: $r5<br>";
+    echo "<form action='deldevice.php' method='post'>";
+    echo "<input type='hidden' name='delete' value='yes'>";
+    echo "<input type='hidden' name='device' value='$r0'>";
+    echo "<input type='submit' value='DELETE DEVICE'>";
+    echo "</form>";
+    echo "</pre></center>";
+
+
+}
+$result->close();
+$conn->close();
+function get_post($conn, $var)
+{
+return $conn->real_escape_string($_POST[$var]);
+}
+?>
+
 </body>
 </html>
+
