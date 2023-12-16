849ad15f7e110d6eea4b26b34233b9bdbfc70efc
eliyas <eliyasson11@gmail.com>
2023-12-17 00:42:34 +0200

    commit

--- frontpage.php
+++ frontpage.php
@@ -146,21 +146,21 @@
     <!-- Three columns of text below the carousel -->
     <div class="row">
       <div class="col-lg-4">
-        <img src="./kuva/kuva 2.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <h2 class="fw-normal">Heading</h2>
-        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+        <img src="./kuva/kuva10.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <h2 class="fw-normal">Tutustu Kokoelmaamme</h2>
+        <p>Sukella viehättävän kyläkirjastomme monipuoliseen kirjakokoelmaan, joka kattaa klassikoita, nykykirjallisuutta ja lastenkirjoja. Olitpa sitten kiinnostunut fiktiosta, tietokirjoista tai lasten kirjoista, meiltä löytyy jotain jokaiselle innokkaalle lukijalle viihtyisiltä hyllyiltämme.</p>
         <p><a class="btn btn-secondary" href="#">View details »</a></p>
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
-        <img src="./kuva/kuva 3.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <h2 class="fw-normal">Heading</h2>
-        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+        <img src="./kuva/kuva 8.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <h2 class="fw-normal"> Yhteisötapahtumat ja -ohjelmat</h2>
+        <p>Osallistu vilkkaan kyläkirjastomme järjestämiin erilaisiin tapahtumiin ja ohjelmiin. Kirjakerhoista ja tarinankerrontahetkistä työpajoihin ja kirjailijavierailuihin, löydä mahdollisuuksia yhdistyä, oppia ja jakaa lukemisen iloa naapureiden ja ystävien kanssa.</p>
         <p><a class="btn btn-secondary" href="#">View details »</a></p>
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
         <img src="./kuva/kuva 5.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <h2 class="fw-normal">Heading</h2>
-        <p>And lastly this, the third column of representative placeholder content.</p>
+        <h2 class="fw-normal"> Kirjaston Palvelut ja Tilat</h2>
+        <p>Tutustu viehättävän kyläkirjastomme tarjoamiin palveluihin ja tiloihin. Tietokoneiden käytöstä tutkimusapuun ja rauhallisiin lukunurkkauksiin sekä mukaviin istuma-alueisiin, löydät monipuoliset palvelut, jotka on suunniteltu tehostamaan lukukokemustasi ja tukemaan elinikäistä oppimista yhteisössämme.</p>
         <p><a class="btn btn-secondary" href="#">View details »</a></p>
       </div><!-- /.col-lg-4 -->
     </div><!-- /.row -->
